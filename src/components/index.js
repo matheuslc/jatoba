783d976f53f7940015b0a006d0a9a153f6f8c8ac
Matheus Carmo <mematheuslc@gmail.com>
2017-03-23 13:30:46 -0300

    feat(index): Entrypoint component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Search from './search'
 
 /**
  * @class
@@ -8,5 +9,15 @@ import React, { Component } from 'react';
 export default class Index extends Component {
   constructor(props) {
     super(props);
+  }
+
+  render() {
+    return (
+        <div className="container-fluid">
+          <Search />
+
+            { this.props.children }
+        </div>
+    )
   }
 }
