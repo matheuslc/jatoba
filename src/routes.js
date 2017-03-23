48984200c46073f692d34ddd0da0855692038ae6
Matheus Carmo <mematheuslc@gmail.com>
2017-03-23 13:58:41 -0300

    feat(routes): Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Index from './components/index';
+import Whether from './components/whether';
 
 export default (
   <Route path="/" component={Index}>
+    <IndexRoute component={Whether} />
   </Route>
 );
