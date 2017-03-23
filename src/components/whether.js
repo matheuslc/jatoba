59feff1cb751d5b63c7670c4ef321b6a6fd078e0
Matheus Carmo <mematheuslc@gmail.com>
2017-03-23 15:49:10 -0300

    feat(whether): Add whether info

--- src/components/whether.js
+++ src/components/whether.js
@@ -6,17 +6,42 @@ import { search } from '../actions/action';
  * @name Whether
  */
 class Whether extends Component {
+  kelvinToCelsius(temperature) {
+    return temperature - 273.15;
+  }
+
   render() {
-    if (!this.props.fetching) {
+    if (this.props.fetching === false) {
       return (
-        <div>
-          { this.props.whether.base }
+        <div className="whether">
+          WHETHER
+          <header className="whether-header">
+            <h1 className="whether-title">
+              { this.props.whether.name },
+              { this.props.whether.sys.country } </h1>
+          </header>
+
+          <section className="whether-details">
+            <ul>
+              <li>Temperatua: { this.kelvinToCelsius(this.props.whether.main.temp) } C</li>
+              <li>Máxima: { this.kelvinToCelsius(this.props.whether.main.temp_max) } C</li>
+              <li>Mínima: { this.kelvinToCelsius(this.props.whether.main.temp_min) } C</li>
+              <li>Umidade: { this.props.whether.main.humidity } %</li>
+            </ul>
+          </section>
+
         </div>
       )
     }
 
+    if (this.props.fetching === true) {
+      return (
+        <div>Loading</div>
+      )
+    }
+
     return (
-      <div>Loading</div>
+      <div>Digite o nome de sua cidade</div>
     )
   }
 }
